b5aaba535e00118ebb500c65dc2165c0d049e832
AlexKondov <alex.kondov@abv.bg>
2014-10-16 17:21:34 +0300

    adding classes

---
+++ TeamEarth/src/com/softuni/earth/base/objects/Orc.java
@@ -0,0 +1,13 @@
+package com.softuni.earth.base.objects;
+
+import javafx.scene.effect.Light.Point;
+
+public class Orc extends Enemy {
+
+	protected Orc(String name, Point position, int healthPoints,
+			int defensePoints, int manaPoints, int range) {
+		super(name, position, healthPoints, defensePoints, manaPoints, range);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ TeamEarth/src/com/softuni/earth/base/objects/Skeleton.java
@@ -0,0 +1,13 @@
+package com.softuni.earth.base.objects;
+
+import javafx.scene.effect.Light.Point;
+
+public class Skeleton extends Enemy {
+
+	protected Skeleton(String name, Point position, int healthPoints,
+			int defensePoints, int manaPoints, int range) {
+		super(name, position, healthPoints, defensePoints, manaPoints, range);
+		// TODO Auto-generated constructor stub
+	}
+
+}
